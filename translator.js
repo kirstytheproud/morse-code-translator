0ac8793d3055b5dcebb1756147cbeb55e51f3c02
kirstytheproud <k.joproud@gmail.com>
2022-02-25 12:55:01 +0000

    started english transtion function

--- translator.js
+++ translator.js
@@ -1,20 +1,69 @@
 
+const englishDisplay = document.querySelector(".input-box--english")
+const morseCodeDisplay = document.querySelector(".input-box--morse")
+
+const englishToMorseButton = document.getElementById("english-to-morse");
+const morseToEnglishButon = document.getElementById("morse-to-english");
+
+let result = "";
+
+const morseCode = {  
+"a": ".-",
+"b": "-...",
+"c": "-.-.",
+"d": "-..",
+"e": ".",
+"f": "..-.",
+"g": "--.",
+"h": "....",
+"i": "..",
+"j": ".---",
+"k": "-.-",
+"l": ".-..",
+"m": "--",
+"n": "-.",
+"o": "---",
+"p": ".--.",
+"q": "--.-",
+"r": ".-.",
+"s": "...",
+"t": "-",
+"u": "..-",
+"v": "...-",
+"w": ".--",
+"x": "-..-",
+"y": "-.--",
+"z": "--..",
+" ": "/",
+}
+
 // stub out functions we need
 
 //english to morse translator function
 export const translateEnglishToMorse = (englishWord) => {
-    console.log("its connected")
+    englishWord = englishDisplay.value
+    const usingSplit = englishWord.split('');
+    console.log(usingSplit);
+    
+    const morseTranslation = usingSplit.map(morse =>{
+        return morse + morseCode;
+    })
+    morseCodeDisplay.value = morseTranslation;
+    //   morseCodeDisplay.value = usingSplit;
+// const morseCodeCharacters = usingSplit.join(' ');
 
 }
 
 
 //english to morse translator function
 export const translateMorseToEnglish = (morseCode) => {
-
+    morseCode = morseCodeDisplay.value
+    englishDisplay.value = morseCodeDisplay.value;
+ 
 }
 
 
-const englishToMorseButton = document.getElementById("english-to-morse");
-const morseToEnglishButon = document.getElementById("morse-to-english");
-
 englishToMorseButton.addEventListener("click", translateEnglishToMorse)
+morseToEnglishButon.addEventListener("click", translateMorseToEnglish)
+
+
